a6ba0012b70ac84355e43b6684fff7072fa5a461
Mikej81 <stumpj@gmail.com>
2022-03-15 19:34:21 -0400

    more cleanup - readme - remove img - cleanup prep

--- example_prep.sh
+++ example_prep.sh
@@ -1,45 +1,4 @@
 #!/usr/bin/env bash
 
-#Need to check OS / Platform
-osName=`uname -s`
-case $osName in
-  Linux*)   export machine="Linux" ;;
-  Darwin*)  export machine="Mac" ;;
-  *)        export machine="UNKNOWN:$osName" ;;
-esac
-
-echo $machine
-
-if [[ "$machine" == "Mac" ]]; then
-  echo "OSX Detected, need to Install / Update Brew and jq..."
-  #Need to update brew and make sure jq is installed to process json
-  echo "updating & upgrading brew..."
-  brew update || brew update
-  brew upgrade
-
-  if brew ls --versions jq > /dev/null; then
-    # The package is installed
-    echo "jq installed proceeding..."
-  else
-    echo "installing jq..."
-    brew install jq
-  fi
-elif [[ "$machine" == "Linux" ]]; then
-  if [ -f /etc/redhat-release ]; then
-    yum -y update
-    yum -y install jq
-  fi
-  if [ -f /etc/lsb-release ]; then
-    apt-get --assume-yes update
-    apt-get --assume-yes install jq
-  fi
-fi
-
-# Map AWS Cred ENV
-
 export VOLT_API_P12_FILE=/api-creds.p12
 export VES_P12_PASSWORD=123456789
-
-#export VOLT_API_CERT=
-#export VOLT_API_KEY=
-#export VOLT_API_URL=
